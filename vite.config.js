510bd24b9936fcd5b12a5d644d72ce4ec34702dd
Alt-Charlie <charlie@alt-design.net>
2026-02-17 13:40:32 +0000

    Tailwind bundling

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import statamic from '@statamic/cms/vite-plugin';
+import tailwindcss from '@tailwindcss/vite'; 
 
 export default defineConfig({
     plugins: [
         laravel({
+            hotFile: 'resources/dist/hot',
             input: [
                 'resources/js/alt-inbound-addon.js',
                 'resources/js/alt-inbound-frontend.js',
@@ -14,5 +16,6 @@ export default defineConfig({
             publicDirectory: 'resources/dist',
         }),
         statamic(),
+        tailwindcss(),
     ],
 });
